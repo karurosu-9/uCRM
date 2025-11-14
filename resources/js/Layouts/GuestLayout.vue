5ebcef7fb8c48c04fedea8598d70686d6b74f50e
carlos <cl0xtc43x.apxma@gmail.com>
2025-11-14 16:49:31 +0900

    ロゴの追加

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -9,7 +9,7 @@ import { Link } from '@inertiajs/vue3';
     >
         <div>
             <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
+                <ApplicationLogo class="h-[180px] w-[180px] fill-current text-gray-500" />
             </Link>
         </div>
 
