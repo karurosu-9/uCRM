5602713aa4a2bcf3b9bce2c56d086c767a73f601
carlos <cl0xtc43x.apxma@gmail.com>
2025-11-27 00:46:55 +0900

    商品の詳細画面の作成

--- resources/js/Pages/Items/Show.vue
+++ resources/js/Pages/Items/Show.vue
@@ -1,31 +1,20 @@
 <script setup>
-import InputError from "@/Components/InputError.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head, router } from "@inertiajs/vue3";
-import { reactive } from "vue";
+import { Head } from "@inertiajs/vue3";
 
 defineProps({
-    errors: Object,
+    item: Object,
 });
 
-const form = reactive({
-    name: null,
-    memo: null,
-    price: null,
-});
-
-const storeItem = () => {
-    router.post("/items", form);
-};
 </script>
 
 <template>
-    <Head title="商品登録" />
+    <Head title="商品詳細" />
 
     <AuthenticatedLayout>
         <template #header>
             <h2 class="text-xl font-semibold leading-tight text-gray-800">
-                商品登録
+                商品詳細
             </h2>
         </template>
 
@@ -39,64 +28,68 @@ const storeItem = () => {
                                     <div class="lg:w-1/2 md:w-2/3 mx-auto">
                                         <div class="flex flex-wrap -m-2">
                                             <div class="p-2 w-full">
-                                                <InputError :message="errors.name" />
                                                 <div class="relative">
                                                     <label
                                                         for="name"
                                                         class="leading-7 text-sm text-gray-600"
                                                         >商品名</label
                                                     >
-                                                    <input
-                                                        type="text"
+                                                    <div
                                                         id="name"
-                                                        name="name"
-                                                        v-model="form.name"
                                                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                                                    />
+                                                    >
+                                                        {{ item.name }}
+                                                    </div>
                                                 </div>
                                             </div>
 
                                             <div class="p-2 w-full">
-                                                <InputError :message="errors.memo" />
                                                 <div class="relative">
                                                     <label
                                                         for="memo"
                                                         class="leading-7 text-sm text-gray-600"
                                                         >メモ</label
                                                     >
-                                                    <textarea
+                                                    <div
                                                         id="memo"
-                                                        name="memo"
-                                                        v-model="form.memo"
                                                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                                                    ></textarea>
-                                                </div>
-                                            </div>
-
-                                            <div class="p-2 w-full">
-                                                <InputError :message="errors.price" />
-                                                <div class="relative">
-                                                    <label
-                                                        for="price"
-                                                        class="leading-7 text-sm text-gray-600"
-                                                        >価格</label
                                                     >
-                                                    <input
-                                                        type="number"
-                                                        id="price"
-                                                        name="price"
-                                                        v-model="form.price"
-                                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                                                    />
+                                                        {{ item.memo }}
+                                                    </div>
                                                 </div>
                                             </div>
 
-                                            <div class="p-2 w-full">
-                                                <button
-                                                    class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                                        </div>
+                                        <div class="p-2 w-full">
+                                            <div class="relative">
+                                                <label
+                                                    for="price"
+                                                    class="leading-7 text-sm text-gray-600"
+                                                    >価格</label
                                                 >
-                                                    商品登録
-                                                </button>
+                                                <div
+                                                    id="price"
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                                >
+                                                    {{ item.price }}
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="p-2 w-full">
+                                            <div class="relative">
+                                                <label
+                                                    for="status"
+                                                    class="leading-7 text-sm text-gray-600"
+                                                    >価格</label
+                                                >
+                                                <div
+                                                    id="status"
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                                >
+                                                    <span v-if="item.is_selling === 1">販売中</span>
+                                                    <span v-if="item.is_selling === 0">停止中</span>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
