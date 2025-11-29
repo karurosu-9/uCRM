8a96bbd156a400a55b62aeda97b7bb2bbdbba42f
carlos <cl0xtc43x.apxma@gmail.com>
2025-11-29 15:32:41 +0900

    顧客関連ファイルの作成とダミーデータの作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -26,5 +27,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class, // UserSeederで作成したダミーデータの呼び出し
             ItemSeeder::class, // ItemSeederで作成したダミーデータの呼び出し
         ]);
+
+        Customer::factory(300)->create(); // CustomerFactoryをもとに300件分のこきゃうのダミーデータの作成
     }
 }
