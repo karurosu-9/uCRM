8d40523543498b6d50131bc695a6285155078d55
carlos <cl0xtc43x.apxma@gmail.com>
2025-12-01 02:34:15 +0900

    顧客編集ページと更新処理の実装

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -68,7 +68,9 @@ class CustomerController extends Controller
      */
     public function edit(Customer $customer)
     {
-        //
+        return Inertia::render('Customers/Edit', [
+            'customer' => $customer
+        ]);
     }
 
     /**
@@ -76,7 +78,21 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->name     = $request->name;
+        $customer->kana     = $request->kana;
+        $customer->tel      = $request->tel;
+        $customer->email    = $request->email;
+        $customer->postcode = $request->postcode;
+        $customer->address  = $request->address;
+        $customer->birthday = $request->birthday;
+        $customer->gender   = $request->gender;
+        $customer->memo     = $request->memo;
+        $customer->save();
+
+        return redirect(route('customers.index'))->with([
+            'status' => 'success',
+            'message' => '顧客情報を更新しました。'
+        ]);
     }
 
     /**
